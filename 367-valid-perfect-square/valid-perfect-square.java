47f748652627dc7a1ed5877ff960a3a4374edd84
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2024-06-27 22:24:12 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (84.65%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int n=1,t=num;
+        
+        double x= Math.pow(num,0.5);
+        return x==(int)x;
+        
+    }    
+}
