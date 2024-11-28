f348f62714b6b53041fab51f2e6e2f8f58d4b548
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2024-11-28 14:24:50 +0530

    Time: 34 ms (16.94%) | Memory: 45.2 MB (17.28%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,17 +1,26 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount+1];
-        Arrays.fill(dp,amount+1);
+    public int solve(int[] coins, int amount, int ind,int[][] dp){
+        if(amount==0)return 0;
+        if(amount<0 || ind<0)return Integer.MAX_VALUE;
+        if(dp[ind][amount]!=-1)return dp[ind][amount];
+         // Two choices: exclude or include the current coin
+        int exclude = solve(coins, amount, ind - 1,dp);
+        int include = solve(coins, amount - coins[ind], ind,dp);
         
-        dp[0]=0;
-        
-        for(int i=1;i<=amount;i++){
-            for(int c: coins){
-                if(i-c>=0){
-                    dp[i]=Math.min(dp[i],1+dp[i-c]);
-                }
-            }
+        // If include is valid, add 1 to account for the coin used
+        if (include != Integer.MAX_VALUE) {
+            include += 1;
         }
-        return dp[amount]==amount+1? -1:dp[amount];
+        
+        // Return the minimum of the two options
+        return dp[ind][amount]=Math.min(exclude, include);
+    }
+    public int coinChange(int[] coins, int amount) {
+        int[][] dp = new int[coins.length+1][amount+1];
+        for(int[] r:dp)
+            Arrays.fill(r,-1);
+        int result = solve(coins, amount, coins.length - 1,dp);
+        
+        return result == Integer.MAX_VALUE ? -1 : result;
     }
 }
