a22f476bea0d3f1068b0c568e7715f36b08011e3
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-04-02 14:15:48 +0530

    Time: 2 ms (34.13%) | Memory: 41.2 MB (44.82%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.java
+++ 377-combination-sum-iv/combination-sum-iv.java
@@ -1,28 +1,30 @@
 class Solution {
-    int res=0;
-    private void dfs(int[] nums, int temp,int target){
-        if(temp>target){
-            return;
+    private int dfs(int[] nums, int target){
+        if(target==0)
+            return 1;
+        if(target<0 )
+            return 0;
+        int sum=0;
+        for(int j =0;j<nums.length;j++){
+            sum+= dfs(nums, target-nums[j]);
         }
-        if(temp==target){
-            res++;
-            return;
+        return sum;
+    }
+    private int tab(int[] nums, int target){
+        int n= nums.length;
+        int[] dp = new int[target+1];
+        dp[0]=1;
+        for(int t= 1;t<=target;t++){
+            for(int q: nums){
+                if(t-q>=0)
+                    dp[t] += dp[t-q]; 
+            }
         }
-        for(int n: nums){
-            dfs(nums, temp+n, target);
-        }
+
+        return dp[target];
 
     }
     public int combinationSum4(int[] nums, int target) {
-        int[] dp = new int[target+1];
-        dp[0]=1;
-        for(int i=1;i<=target;i++){
-            for(int n: nums){
-                if(n<=i){
-                    dp[i]+=dp[i-n];
-                }
-            }
-        }
-        return dp[target];
+        return tab( nums, target);
     }
 }
