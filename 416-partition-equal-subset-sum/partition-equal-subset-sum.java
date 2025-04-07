02ba6b07a23ca4280d7d92f058afbc8841866020
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-04-07 10:32:59 +0530

    Time: 1203 ms (5.01%) | Memory: 233.5 MB (5.07%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,37 +1,26 @@
 class Solution {
-    private boolean subset(int i, int[] nums, int target){
+    HashMap<String, Boolean> map = new HashMap<>();
+
+    private boolean check(int i,int target, int[] nums){
+        if(i==nums.length)
+            return false;
+        String key = i+"," +target;
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
         if(target==0)
             return true;
-        if(i>=nums.length)
-            return false;
-        
-        return subset(i+1, nums, target-nums[i]) || subset(i+1, nums, target);
-
-    }
-    private boolean tab(int[] nums, int target){
-        int n=nums.length;
-        boolean[][] dp = new boolean[n][target+1];
-        for(int i=0;i<n;i++){
-            dp[i][0]=true;
-        }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=target;j++){
-                dp[i][j] = dp[i-1][j];
-                if(j-nums[i]>=0){
-                    dp[i][j] = dp[i][j] || dp[i-1][j-nums[i]];
-                }
-            }
-        }
-
-        return dp[n-1][target];
+        boolean temp = check(i+1, target-nums[i], nums) || check(i+1, target, nums);
+        map.put(key, temp);
+        return temp;
     }
     public boolean canPartition(int[] nums) {
-        int sum =0;
+        int s=0;
         for(int i: nums){
-            sum+=i;
+            s+=i;
         }
-        if(sum%2!=0)
+        if(s%2==1)
             return false;
-        return tab(nums, sum/2);
+        return check(0, s/2, nums);
     }
 }
