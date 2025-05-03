d4af97f5cdbb4f4aecd685945f4dd55b4f4d7915
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-04 02:17:45 +0530

    Time: 4 ms (36.47%) | Memory: 44.7 MB (65.35%) - LeetSync

---
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -0,0 +1,35 @@
+class Solution {
+    private boolean dfs(int node, int parent, List<List<Integer>> adj, boolean[] vis){
+        vis[node]=true;
+        for(int nei : adj.get(node)){
+            if(vis[nei] && nei!=parent){
+                return false;
+            }else if(vis[nei] && nei==parent){
+                continue;
+            }else{
+                if(dfs(nei, node, adj, vis)==false)
+                    return false;
+            }
+
+        }
+        return true;
+    }
+    public boolean validTree(int n, int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] e: edges){
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+        }
+        boolean[] vis = new boolean[n];
+
+        boolean ans=  dfs(0,-1, adj, vis);
+        for(int i=0;i<n;i++){
+            if(vis[i]==false)
+                return false;
+        }
+        return ans;
+    }
+}
