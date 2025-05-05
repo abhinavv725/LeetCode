60a033117784e03c7fa6a6de90dedbdbf010eec3
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-05 22:37:37 +0530

    Time: 19 ms (56.46%) | Memory: 45.2 MB (45.90%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,27 +1,30 @@
 class Solution {
-    private String check(int l, int r, int n,String s){
-        if(l<0 || r>=n || s.charAt(l)!=s.charAt(r)){
-            return s.substring(l+1,r);
+    String ans ="";
+    private String longest(int l, int r, String s){
+        while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+            l--;
+            r++;
         }
-        return check(l-1, r+1,n, s);
-
-        
+        return s.substring(l+1, r);
 
     }
     public String longestPalindrome(String s) {
-        
-        int n = s.length();
-        String ans=String.valueOf(s.charAt(0));
-        for(int i=0;i<n;i++){
-            String check =check(i,i, n, s);
-            String check1 =check(i,i+1, n, s);
-            if(check.length()>= ans.length()){
-                ans=check;
+        for(int i=0;i<s.length();i++){
+            String temp1 = longest(i, i, s);
+            String temp2 = longest(i, i+1, s);
+
+            if(temp1.length()>ans.length()){
+                ans=temp1;
+
             }
-            if(check1.length()>= ans.length()){
-                ans=check1;
+            if(temp2.length()>ans.length()){
+                ans=temp2;
+                
             }
+
         }
+
         return ans;
+        
     }
 }
