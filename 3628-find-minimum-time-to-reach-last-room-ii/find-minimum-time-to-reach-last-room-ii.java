6a8a8ff5d847c8bc567243bde275cdd84c09038a
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-08 12:45:01 +0530

    Time: 443 ms (29.08%) | Memory: 112.9 MB (37.01%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
@@ -1,58 +1,46 @@
 class Solution {
-    private class Node{
-        int i;int j;int time;int step;
-        Node(int i, int j, int time, int step){
-            this.i=i;
-            this.j=j;
-            this.time=time;
-            this.step=step;
-        }
-    }
-    private boolean isValid(int i,int j,int n,int m){
-        return i>=0 && j>=0 && i<n && j<m;
+    private boolean isValid(int i, int j, int n, int m){
+        return i>=0 && j>=0 && i< n && j<m;
     }
     public int minTimeToReach(int[][] moveTime) {
-        PriorityQueue<Node> pq = new PriorityQueue<>((a,b)-> Integer.compare(a.time,b.time));
         int n= moveTime.length;
-        int m= moveTime[0].length;
-        boolean[][] vis = new boolean[n][m];
-        pq.add(new Node(0,0,0,0));
-        int[][] dir = new int[][] {{0,1},{1,0},{0,-1},{-1,0}};
-        int[][] dist = new int[n][m];
-        for (int[] d : dist) {
-            Arrays.fill(d, Integer.MAX_VALUE);
+        int m = moveTime[0].length;
+        int[][] time = new int[n][m];
+        for(int []t: time){
+            Arrays.fill(t, Integer.MAX_VALUE);
         }
-        dist[0][0]=0;
+        time[0][0]=0;
+        int[][] directions = {{0,1}, {1,0}, {0,-1},{-1,0}};
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[2], b[2]));
+        pq.add(new int[] {0,0,0, 0});
         while(!pq.isEmpty()){
-            Node p = pq.poll();
-            int i=p.i;
-            int j=p.j;
-            int time=p.time;
-            int step = p.step;
-            if(time>dist[i][j])
+            int[] curr = pq.poll();
+            int currI = curr[0];
+            int currJ = curr[1];
+            int currT = curr[2];
+            int step = curr[3];
+            if(currT> time[currI][currJ])
                 continue;
-            for(int[] d: dir){
-                int nr = i+d[0];
-                int nc = j+d[1];
-                if(!isValid(nr,nc,n,m))
-                    continue;
-                int nt = 1+Math.max(time, moveTime[nr][nc]);
-                if(step==1){
-                    nt++;
-                }
-                if(dist[nr][nc]>nt){
-                    dist[nr][nc]=nt;
+            for(int[] dir: directions){
+                int nI = currI+dir[0];
+                int nJ = currJ+dir[1];
+                if(isValid(nI, nJ, n,m)){
+                    int nT = 1+ Math.max(currT, moveTime[nI][nJ]);
                     if(step==1){
-                        pq.add(new Node(nr,nc, nt, 2));
+                        nT++;
+                    }
+                    if(nT<time[nI][nJ]){
+                        time[nI][nJ]=nT;
+                        if(step==1){
+                            pq.add(new int[] {nI,nJ, nT, 2});
+                        }else{
+                            pq.add(new int[] {nI,nJ, nT, 1});
 
-                    }else{
-                        pq.add(new Node(nr,nc, nt, 1));
-
+                        }
                     }
                 }
             }
-            
         }
-        return dist[n-1][m-1];
+        return time[n-1][m-1];
     }
 }
