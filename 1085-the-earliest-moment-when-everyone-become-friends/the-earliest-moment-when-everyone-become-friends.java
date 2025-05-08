572bb86cb1d10ec958c3235399f14b39d6c9b3a4
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-09 01:30:51 +0530

    Time: 5 ms (98.99%) | Memory: 44.6 MB (54.30%) - LeetSync

--- 1085-the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.java
+++ 1085-the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.java
@@ -1,46 +1,41 @@
 class Solution {
-    int[] parent;
+    int[] parent ;
     int[] rank;
-    private void union(int x, int y){
-        int rootA = find(x);
-        int rootB = find(y);
-
-        if(rootA==rootB)
-            return;
-        else if(rootA>rootB){
-            parent[rootB] = rootA;
-            rank[rootA]+=rank[rootB];
-        }else{
-            parent[rootA] = rootB;
-            rank[rootB]+=rank[rootA];
-        }
-    }
     private int find(int x){
-        if(parent[x]==-1)
+        if(parent[x]==-1){
             return x;
+        }
         return parent[x] = find(parent[x]);
     }
+    private boolean union(int a, int b){
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if(rootA==rootB)
+            return false;
+        if(rootA>rootB){
+            parent[rootB]=rootA;
+            rank[rootA]+=rank[rootB];
+        }else{
+            parent[rootA]=rootB;
+            rank[rootB]+=rank[rootA];
+        }
+
+        return true;
+    }
     public int earliestAcq(int[][] logs, int n) {
-        Arrays.sort(logs, (a,b)-> Integer.compare(a[0], b[0]));
+        Arrays.sort(logs, (a,b) -> Integer.compare(a[0], b[0]));
         parent = new int[n];
         rank = new int[n];
         Arrays.fill(parent, -1);
-
+        int groups= n;
         for(int[] l: logs){
-            union(l[1], l[2]);
-            int p = find(0);
-            boolean allFriend=true;
-            for(int i=1;i<parent.length;i++){
-                if(find(i)!=p){
-                    allFriend=false;
-                    break;
-                }
-            }
-            if(allFriend){
+            if(union(l[1], l[2]))
+                groups--;
+            
+            if(groups==1)
                 return l[0];
-            }
         }
         return -1;
-
     }
 }
