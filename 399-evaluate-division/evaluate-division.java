7a112fcca85ca813ee7d2d0c6c55f1518a2b4865
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-09 21:00:44 +0530

    Time: 1 ms (99.14%) | Memory: 42.2 MB (71.94%) - LeetSync

--- 399-evaluate-division/evaluate-division.java
+++ 399-evaluate-division/evaluate-division.java
@@ -1,47 +1,53 @@
 class Solution {
-    private double dfs(HashMap<String ,HashMap<String, Double>> adj, String a, String b, double product,Set<String> visited ){
-
-        if(a.equals(b)){
+    
+    private double dfs(HashMap<String, HashMap<String, Double>> adj, String a , String b, double product, Set<String> visited){
+        if(a.equals(b))
             return product;
-        }
         visited.add(a);
-        for(Map.Entry<String, Double> nei: adj.get(a).entrySet()){
-            if(visited.contains(nei.getKey())==false){
-                double result = dfs(adj, nei.getKey(), b, product*nei.getValue(), visited);
-                if(result!=-1.0)
-                    return result;
+        HashMap<String, Double> cities = adj.get(a);
+        for(String key: cities.keySet()){
+            if(!visited.contains(key)){
+                double temp = dfs(adj, key, b, product * cities.get(key), visited);
+                if(temp!=-1.0)
+                    return temp;
             }
-        }
-        return -1;
 
+        }
+        return -1.0;
     }
+
+
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        HashMap<String, HashMap<String, Double>> map = new HashMap<>();
-        for(int i=0;i<equations.size();i++){
-            double value = values[i];
+        HashMap<String, HashMap<String, Double>> adj = new HashMap<>();
+        for(int i=0;i<values.length;i++){
+            double val = values[i];
             String a = equations.get(i).get(0);
             String b = equations.get(i).get(1);
-
-            map.putIfAbsent(a,new HashMap<>());
-            map.putIfAbsent(b,new HashMap<>());
-            map.get(a).put(b, value);
-            map.get(b).put(a, 1.0/value);
+            if(!adj.containsKey(a)){
+                adj.put(a, new HashMap<>());
+            }
+            if(!adj.containsKey(b)){
+                adj.put(b, new HashMap<>());
+            }
+            adj.get(a).put(b, val);
+            adj.get(b).put(a, 1.0/val);
         }
+
         int n = queries.size();
         double[] ans = new double[n];
-
         for(int i=0;i<n;i++){
             String a = queries.get(i).get(0);
             String b = queries.get(i).get(1);
-
-            if(!map.containsKey(a) || !map.containsKey(b)){
+            if(!adj.containsKey(a) || !adj.containsKey(b)){
                 ans[i]=-1.0;
             }else{
-                Set<String> visited = new HashSet<>();
-                ans[i] = dfs(map, a,b, 1.0, visited);
+                Set<String> visit = new HashSet<>();
+                ans[i]=dfs(adj, a, b, 1.0, visit);
             }
         }
 
         return ans;
-     }
+        
+        
+    }
 }
