ce9a867c9d3592188b752c92054f91821372d069
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-10 05:01:57 +0530

    Time: 81 ms (83.08%) | Memory: 44.9 MB (56.44%) - LeetSync

--- 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
@@ -1,25 +1,43 @@
 class Solution {
-    public int maximumDetonation(int[][] bombs) {
-        int ans=0;
-        for(int i=0;i<bombs.length;i++){
-            ans=Math.max(ans, dfs(i, new boolean[bombs.length], bombs));
-        }
-        return ans;
-        
+    private boolean inRange(int[] a, int[]b){
+        long dx = a[0]-b[0];
+        long dy =a[1]-b[1];
+        long distSqr = dx*dx + dy*dy;
+        long radius = (long)a[2]*a[2];
+        return distSqr<=radius;
     }
-    private int dfs(int idx, boolean[] vis, int[][] bombs){
-        vis[idx]=true;
+    private int dfs(int idx, List<List<Integer>> adj, boolean[] vis){
+        vis[idx] =true;
         int count=1;
-        int n = bombs.length;
-        for(int i=0;i<n;i++){
-            if(!vis[i] && inRange(bombs[idx], bombs[i])){
-                count+=dfs(i, vis, bombs);
+        for(int nei: adj.get(idx)){
+            if(!vis[nei]){
+                count += dfs(nei, adj, vis);
             }
         }
         return count;
     }
-    private boolean inRange(int[] a, int[] b){
-        long dx = a[0]-b[0], dy = a[1]-b[1], r = a[2];
-        return dx*dx + dy*dy <= r*r;
+    public int maximumDetonation(int[][] bombs) {
+        List<List<Integer>> adj = new ArrayList<>();
+        int n = bombs.length;
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && inRange(bombs[i], bombs[j])){
+                    adj.get(i).add(j);
+                }
+            }
+        }
+        int ans = 0;
+
+        for(int i=0;i<n;i++){
+            boolean[] vis = new boolean[n];
+            ans = Math.max(ans, dfs(i,adj,vis));
+
+        }
+        return ans;
+        
     }
 }
