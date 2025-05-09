dac3fdcd95cff437dfd49f17d06a83585a6c5288
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-10 00:59:16 +0530

    Time: 2 ms (84.24%) | Memory: 45.6 MB (38.50%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,52 +1,44 @@
 class Solution {
-    
-    private void dfs(int i,int j,char[][] board,boolean[][] vis){
-        
+    int[][] directions = {{0,1}, {1,0}, {0,-1},{-1,0}};
+    private boolean isValid(int i, int j, int n, int m){
+        return i>=0 && j>=0 && i<n && j<m;
+
+    }
+    private void dfs(int i, int j, char[][] board, boolean[][] vis){
         vis[i][j]=true;
-        int n=board.length;
-        int m=board[0].length;
-        int[] drow = {-1,0,1,0};
-        int[] dcol = {0,1,0,-1};
-        for(int t=0;t<4;t++){
-            int nrow=i+drow[t];
-            int ncol=j+dcol[t];
-            
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==false && board[nrow][ncol]=='O'){
-                dfs(nrow,ncol,board,vis);
+        for(int[] dir: directions){
+            int ni = dir[0]+i;
+            int nj = dir[1]+j;
+            if(isValid(ni, nj, board.length, board[0].length) && !vis[ni][nj] && board[ni][nj]=='O'){
+                dfs(ni, nj, board, vis);
             }
         }
     }
-    
-    
     public void solve(char[][] board) {
-        int n=board.length;
-        int m=board[0].length;
-        
-        boolean[][] vis = new boolean[n][m];
-        
-        //traverse the first and last row
-        for(int i=0;i<m;i++){
-            if(board[0][i]=='O' && vis[0][i]==false){
-                dfs(0,i,board,vis);
-            }
-            if(board[n-1][i]=='O' && vis[n-1][i]==false){
-                dfs(n-1,i,board,vis);
-            }
-            
-        }
-        //traverse the first and last col
+        int n = board.length;
+        int m = board[0].length;
+        boolean[][] vis =new boolean[n][m];
         for(int i=0;i<n;i++){
-            if(board[i][0]=='O'&& vis[i][0]==false){
-                dfs(i,0,board,vis);
+            if(board[i][0] == 'O'){
+                dfs(i, 0, board, vis);
             }
-            if(board[i][m-1]=='O' && vis[i][m-1]==false){
-                dfs(i,m-1,board,vis);
+            if(board[i][m-1]=='O'){
+                dfs(i, m-1, board, vis);
             }
         }
-        
+        for(int j=0;j<m;j++){
+            if(board[0][j] == 'O'){
+                dfs(0, j, board, vis);
+                
+            }
+            if(board[n-1][j]=='O'){
+                dfs(n-1, j, board, vis);
+                
+            }
+        }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j]=='O' && vis[i][j]==false){
+                if(board[i][j]=='O' && !vis[i][j]){
                     board[i][j]='X';
                 }
             }
