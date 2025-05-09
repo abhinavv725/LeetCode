46ccd1ec2daaeb432fa270622bf2aa6b50b4f76f
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-09 23:28:15 +0530

    Time: 17 ms (22.74%) | Memory: 47.5 MB (41.55%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,60 +1,41 @@
-class Pair{
-    int i;
-    int j;
-    int step;
-    Pair(int _i,int _j, int _step){
-        this.i=_i;
-        this.j=_j;
-        this.step=_step;
-    }
-}
-
-
 class Solution {
-    
+    int[][] directions = {{0,1}, {1,0},{0,-1},{-1,0}};
+    private boolean isValid(int i, int j, int n, int m){
+        return i>=0 && j>=0 && i<n && j<m;
+    }
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length;
-        int m = mat[0].length;
-        
-        int[][] ans= new int[n][m];
+        int m=mat[0].length;
+        int[][] ans = new int[n][m];
+        for(int[] a: ans){
+            Arrays.fill(a, (int)1e9);
+        }
         boolean[][] vis = new boolean[n][m];
-        
-        Queue<Pair> q = new LinkedList<>(); 
-        
+        Queue<int[]> q = new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
+                    q.add(new int[] {i, j, 0});
                     vis[i][j]=true;
-                    q.add(new Pair(i,j,0));
+                    
                 }
             }
         }
-        int[]drow = {-1,0,1,0};
-        int []dcol = {0,1,0,-1};
-        
         while(!q.isEmpty()){
-            int i = q.peek().i;
-            int j=q.peek().j;
-            int step = q.peek().step;
-            
-            q.remove();
-            ans[i][j]=step;
-            for(int t=0;t<4;t++){
-                int nrow=i+drow[t];
-                int ncol=j+dcol[t];
-                
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol< m && vis[nrow][ncol]==false){
-                    vis[nrow][ncol]=true;
-                    q.add(new Pair(nrow,ncol,step+1));
+            int size = q.size();
+            while(size-- >0){
+                int[] curr = q.poll();
+                int i = curr[0], j=curr[1], dist = curr[2];
+                ans[i][j] = dist;
+                for(int[] dir: directions){
+                    int ni = i+dir[0], nj = j+dir[1];
+                    if(isValid(ni, nj, n,m) && mat[ni][nj] ==1 && !vis[ni][nj]){
+                        q.add(new int[] {ni, nj, dist+1});
+                        vis[ni][nj]=true;
+                    }
                 }
-                
             }
-            
-            
         }
-        
-        
-        
         return ans;
         
     }
