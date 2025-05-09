50b6d0c5550bf7d944900c2a950f4c8bce95b253
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-09 18:30:18 +0530

    Time: 15 ms (69.40%) | Memory: 45.3 MB (91.80%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,49 +1,35 @@
-
 class Solution {
-    private class Node{
-        int i, j, len;
-        Node(int i, int j, int len){
-            this.i=i;
-            this.j=j;
-            this.len=len;
-        }
-    }
     private boolean isValid(int i, int j, int n){
-        if(i>=0 && j>=0 && i<n && j<n){
-            return true;
-        }
-        return false;
+        return i>=0 && j>=0 && i<n && j<n;
     }
-    
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int n =grid.length;
-        Queue<Node> q = new LinkedList<>();
-        boolean[][] vis= new boolean[n][n];
-        q.add(new Node(0,0,1));
+        if(grid[0][0]==1)
+            return -1;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0,1});
+        int n=grid.length;
+        boolean[][] vis = new boolean[n][n];
         vis[0][0]=true;
-        if(grid[0][0]==1)return -1;
         if(grid[0][0]==0 && n==1)return 1;
         while(!q.isEmpty()){
-            Node x = q.poll();
-            int i=x.i, j=x.j, len=x.len;
-
-            for(int a=-1;a<=1;a++){
-                for(int b=-1;b<=1;b++){
-                    int dx = i+a;
-                    int dy = j+b;
-                    if(isValid(dx,dy,n) && !vis[dx][dy] && grid[dx][dy]==0){
-                        vis[dx][dy]=true;
-                        q.add(new Node(dx,dy, len+1));
-                        if(dx==n-1 && dy==n-1){
-                            return len+1;
+            int size=q.size();
+            while(size-- >0){
+                int[] curr = q.poll();
+                for(int i=-1;i<=1;i++){
+                    for(int j=-1;j<=1;j++){
+                        int ni = curr[0]+i;
+                        int nj = curr[1]+j;
+                        if(isValid(ni, nj, n) && grid[ni][nj]==0 && !vis[ni][nj]){
+                            vis[ni][nj]=true;
+                            q.add(new int[]{ni, nj, curr[2]+1});
+                            if(ni==n-1 && nj==n-1){
+                                return curr[2]+1;
+                            }
                         }
                     }
-
                 }
             }
-            
-
-        } 
+        }
         return -1;
 
     }
