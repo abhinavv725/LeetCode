d4f1301eb50a4302dc5e8957bef468551e29124a
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-10 16:36:47 +0530

    Time: 40 ms (12.88%) | Memory: 45.7 MB (74.41%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,34 +1,35 @@
 class Solution {
+    
     public int leastInterval(char[] tasks, int n) {
-        int[] freq = new int[26];
-        for(char c: tasks){
-            freq[c-'A']++;
+        int[] arr = new int[26];
+        for(char c:tasks){
+            arr[c-'A']++;
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> Integer.compare(b,a));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> Integer.compare(b,a));
         for(int i=0;i<26;i++){
-            if(freq[i]>0){
-                pq.add(freq[i]);
-
+            if(arr[i]>0){
+                pq.add(arr[i]);
             }
         }
-        int time=0;
-        // new int[]{num, time};
         Queue<int[]> q = new LinkedList<>();
+        int time=0;
         while(!pq.isEmpty() || !q.isEmpty()){
             time++;
-            if(!q.isEmpty() && q.peek()[1]==time){
+            while(!q.isEmpty() && q.peek()[1]==time){
                 pq.add(q.poll()[0]);
             }
             if(!pq.isEmpty()){
-                int x = pq.poll();
-                if(x==1)
+                int curr = pq.poll();
+                if(curr==1)
                     continue;
                 else{
-                    q.add(new int[]{x-1, time+n+1});
-                }
+                    q.add(new int[] {curr-1, time+n+1});
 
+                }
             }
+
         }
         return time;
+
     }
 }
