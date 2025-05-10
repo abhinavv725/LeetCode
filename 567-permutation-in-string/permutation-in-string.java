9a52bb2ffa83675d02f94154f12bcea29a3471b8
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-11 00:29:18 +0530

    Time: 23 ms (40.29%) | Memory: 45 MB (18.63%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,36 +1,32 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length()> s2.length())
-            return false;
+        if(s1.length()>s2.length())   return false;
+        if(s1.equals(s2))   return true;
         HashMap<Character, Integer> m1 = new HashMap<>();
+        HashMap<Character, Integer> m2 = new HashMap<>();
         for(char c: s1.toCharArray()){
             m1.put(c, m1.getOrDefault(c, 0)+1);
         }
-        HashMap<Character, Integer> m2 = new HashMap<>();
-        int l=0, r = s1.length();
-        
-        for(int i=0;i<r;i++){
+        for(int i=0;i<s1.length();i++){
             char c = s2.charAt(i);
             m2.put(c, m2.getOrDefault(c, 0)+1);
         }
-        if(m1.equals(m2))
-            return true;
-        while(l<=r && r<s2.length()){
-            
-            char newC = s2.charAt(r);
-            char oldC =s2.charAt(l);
+        if(m1.equals(m2))   return true;
+        int i=s1.length(), k=s1.length();
+
+        while(i<s2.length()){
+            char last = s2.charAt(i-k);
+            char newC = s2.charAt(i);
+            m2.put(last, m2.get(last)-1);
             m2.put(newC, m2.getOrDefault(newC, 0)+1);
-            m2.put(oldC, m2.get(oldC)-1);
-            if(m2.get(oldC)==0){
-                m2.remove(oldC);
-            }
+            if(m2.get(last)==0)
+                m2.remove(last);
+            if(m1.equals(m2))   return true;
+            i++;
 
-
-            if(m1.equals(m2))
-                return true;
-            l++;r++;
-            
         }
         return false;
+
+
     }
 }
