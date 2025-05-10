10e876888c194af9ef00191e53dbf91e95d4d81b
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-10 22:40:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (64.42%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,27 +1,11 @@
 class Solution {
-    List<String> ans = new ArrayList<>();
-    HashMap<Character, String> map = new HashMap<>();
-    
-    StringBuilder sb = new StringBuilder();
-    private void dfs(int i, String digits){
-        if(i==digits.length()){
-            if(sb.length()>0){
-                ans.add(sb.toString());
-            }
-            return;
-        }
-        String letter = map.get(digits.charAt(i));
-        for(char c: letter.toCharArray()){
-            sb.append(c);
-            dfs(i+1, digits);
-
-            sb.deleteCharAt(sb.length()-1);
-
-        }
-        
-
-    }
-    public List<String> letterCombinations(String digits) {
+    List<String> ans;
+    HashMap<Character, String> map;
+    StringBuilder sb;
+    Solution(){
+        ans = new ArrayList<>();
+        map = new HashMap<>();
+        sb = new StringBuilder();
         map.put('2',"abc");
         map.put('3',"def");
         map.put('4',"ghi");
@@ -30,10 +14,29 @@ class Solution {
         map.put('7',"pqrs");
         map.put('8',"tuv");
         map.put('9',"wxyz");
-        
-        dfs(0, digits);
 
+    }
+    private void dfs(int i, String digits){
+        if(i==digits.length()){
+            if(sb.length()!=0)
+                ans.add(sb.toString());
+            return;
+        }
+        char curr = digits.charAt(i);
+        String keys = map.get(curr);
+        for(char c: keys.toCharArray()){
+            sb.append(c);
+            dfs(i+1, digits);
+
+            sb.deleteCharAt(sb.length()-1);
+            // dfs(i+1, digits);
+
+        }
+
+    }
+
+    public List<String> letterCombinations(String digits) {
+        dfs(0, digits);
         return ans;
-        
     }
 }
