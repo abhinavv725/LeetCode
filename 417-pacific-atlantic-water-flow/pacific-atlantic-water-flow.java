f109e7bd2d44579d79ce0874f21ad5cc0cf069bc
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-10 06:44:36 +0530

    Time: 4 ms (97.85%) | Memory: 45.7 MB (49.54%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,58 +1,46 @@
 class Solution {
-    private class Pair{
-        int i;int j;
-        Pair(int i, int j){
-            this.i=i;
-            this.j=j;
-        }
-    }
-    private boolean isValid(int i,int j, int n, int m){
+    private boolean isValid(int i, int j, int n, int m){
         return i>=0 && j>=0 && i<n && j<m;
     }
-    private void fillTable(int i,int j, int n, int m, boolean[][] visit, 
-                            int prev,int[][] grid){
-        
-        if(!isValid(i,j,n,m) || prev > grid[i][j] || visit[i][j] )
-            return;
-        visit[i][j]=true;
-        fillTable(i+1,j,n,m,visit, grid[i][j], grid);
-        fillTable(i,j+1,n,m,visit, grid[i][j], grid);
-        fillTable(i-1,j,n,m,visit, grid[i][j], grid);
-        fillTable(i,j-1,n,m,visit, grid[i][j], grid);
-        
-
+    int[][] directions = {{-1, 0},{0,-1},{1,0},{0,1}};
+    private void dfs(int i, int j,int[][] heights, boolean[][] vis){
+        int n = heights.length;
+        int m = heights[0].length;
+        vis[i][j]=true;
+        for(int[] dir: directions){
+            int ni = i+dir[0];
+            int nj = j+dir[1];
+            if(isValid(ni, nj, n,m) && !vis[ni][nj] && heights[ni][nj]>=heights[i][j]){
+                dfs(ni, nj, heights, vis);
+            }
+        }
     }
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        int n=heights.length;
-        int m=heights[0].length;
+        int n = heights.length;
+        int m = heights[0].length;
 
-        boolean[][] atl = new boolean[n][m]; 
         boolean[][] pac = new boolean[n][m];
-
-         // Traverse Pacific (top and left boundaries)
-        for (int i = 0; i < n; i++) {
-            fillTable(i, 0, n, m, pac, heights[i][0], heights); // Left boundary
-        }
-        for (int j = 0; j < m; j++) {
-            fillTable(0, j, n, m, pac, heights[0][j], heights); // Top boundary
-        }
-
-        // Traverse Atlantic (bottom and right boundaries)
-        for (int i = 0; i < n; i++) {
-            fillTable(i, m - 1, n, m, atl, heights[i][m - 1], heights); // Right boundary
-        }
-        for (int j = 0; j < m; j++) {
-            fillTable(n - 1, j, n, m, atl, heights[n - 1][j], heights); // Bottom boundary
-        }
+        boolean[][] atl = new boolean[n][m];
 
         for(int i=0;i<n;i++){
+            if(!pac[i][0]) dfs(i, 0, heights, pac);
+            if(!atl[i][m-1]) dfs(i, m-1, heights, atl);
+        }
+        for(int j=0;j<m;j++){
+            if(!pac[0][j]) dfs(0, j, heights, pac);
+            if(!atl[n-1][j]) dfs(n-1, j, heights, atl);
+        }
+
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(atl[i][j] && pac[i][j]){
-                    ans.add(Arrays.asList(i,j));
+                if(pac[i][j] && atl[i][j]){
+                    ans.add(Arrays.asList(i, j));
                 }
             }
         }
         return ans;
+
     }
 }
