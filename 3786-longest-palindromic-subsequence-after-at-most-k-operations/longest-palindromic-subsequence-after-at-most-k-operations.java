23444e29a02dd8486a26092832056771eeb8319e
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-11 09:59:19 +0530

    Time: 232 ms (16.82%) | Memory: 80.9 MB (59.91%) - LeetSync

--- 3786-longest-palindromic-subsequence-after-at-most-k-operations/longest-palindromic-subsequence-after-at-most-k-operations.java
+++ 3786-longest-palindromic-subsequence-after-at-most-k-operations/longest-palindromic-subsequence-after-at-most-k-operations.java
@@ -1,29 +1,26 @@
 class Solution {
-    private int dfs(int l, int r, int operations, String s,int[][][] dp ){
+    private int dfs(int l, int r, int operations, String s, int[][][] dp){
         if(l==r)    return 1;
         if(l>r)     return 0;
-        if(dp[l][r][operations]!=-1)
-            return dp[l][r][operations];
-        int moveL = dfs(l+1, r, operations,s, dp);
-        int moveR = dfs(l, r-1, operations,s, dp);
-        int operationsRequired = Math.min(
-                                    Math.abs(s.charAt(l)-s.charAt(r)),
-                                    26-Math.abs(s.charAt(l)-s.charAt(r))
-                                );
-        int convert = Integer.MIN_VALUE; 
-        if(operations>=operationsRequired){
-            convert = 2+ dfs(l+1, r-1, operations-operationsRequired,s, dp);
+        if(dp[l][r][operations]!=-1)    return dp[l][r][operations];
+        int moveL = dfs(l+1, r, operations-0, s, dp);
+        int moveR = dfs(l, r-1, operations-0, s,dp);
+        int convert=Integer.MIN_VALUE;
+        int opsRequired = Math.min(Math.abs(s.charAt(l)-s.charAt(r)),
+                                    26 - Math.abs(s.charAt(l)-s.charAt(r)));
+        if(opsRequired<=operations){
+            convert = 2+ dfs(l+1, r-1, operations-opsRequired, s, dp);
         }
-        return dp[l][r][operations]= Math.max(moveL, Math.max(moveR, convert));
+        return dp[l][r][operations]=Math.max(moveL, Math.max(moveR, convert));
     }
-
     public int longestPalindromicSubsequence(String s, int k) {
-        int[][][] dp = new int[s.length()][s.length()][k+1];
+        int n=s.length();
+        int[][][] dp = new int[n][n][k+1];
         for(int[][] d2: dp){
-            for(int[] d1: d2){
+            for(int[]d1: d2){
                 Arrays.fill(d1, -1);
             }
         }
-        return dfs(0, s.length()-1,k, s, dp);
+        return dfs(0, n-1, k, s, dp);
     }
 }
