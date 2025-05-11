d1187c9f8faf5f9988e3f65ac13455abebf41977
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-11 13:02:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (45.64%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,18 +1,20 @@
 class Solution {
-    private double findPower(double x, long n){
+    private double find(double x, long n){
         if(x==0)    return 0;
         if(n==0)    return 1;
-
-        double res = findPower(x, n/2);
+        
+        double res = find(x, n/2);
         res=res*res;
-        if(n%2==1){
-            res *= x;
-        }
+        if(n%2==1)
+            res*=x;
         return res;
+
+
     }
     public double myPow(double x, int n) {
-        long N=n;
-        double result = findPower(x, Math.abs(N));
-        return (n>=0) ? result : 1/result;
+        long N = Math.abs((long)n);
+        double res = find(x, N);
+        return n<0 ? 1/res : res;
+
     }
 }
