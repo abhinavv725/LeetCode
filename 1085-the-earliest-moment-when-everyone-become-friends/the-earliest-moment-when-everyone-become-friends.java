8d6c3ac5395ceea52144433a1312cdf88e357601
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-11 15:32:08 +0530

    Time: 5 ms (98.91%) | Memory: 44.6 MB (66.39%) - LeetSync

--- 1085-the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.java
+++ 1085-the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.java
@@ -1,5 +1,5 @@
 class Solution {
-    int[] parent ;
+    int[] parent;
     int[] rank;
     private int find(int x){
         if(parent[x]==-1){
@@ -10,31 +10,31 @@ class Solution {
     private boolean union(int a, int b){
         int rootA = find(a);
         int rootB = find(b);
-
-        if(rootA==rootB)
-            return false;
+        if(rootA==rootB)    return false;
+        
         if(rootA>rootB){
-            parent[rootB]=rootA;
+            parent[rootB] = rootA;
             rank[rootA]+=rank[rootB];
         }else{
-            parent[rootA]=rootB;
+            parent[rootA] = rootB;
             rank[rootB]+=rank[rootA];
         }
 
         return true;
     }
     public int earliestAcq(int[][] logs, int n) {
-        Arrays.sort(logs, (a,b) -> Integer.compare(a[0], b[0]));
-        parent = new int[n];
-        rank = new int[n];
+        parent=new int[n];
+        rank=new int[n];
         Arrays.fill(parent, -1);
-        int groups= n;
-        for(int[] l: logs){
-            if(union(l[1], l[2]))
-                groups--;
-            
-            if(groups==1)
-                return l[0];
+        Arrays.sort(logs, (a, b)-> Integer.compare(a[0], b[0]));
+        int group =n;
+        for(int[] log: logs){
+            if(union(log[1], log[2])){
+                group--;
+            }
+            if(group==1){
+                return log[0];
+            }
         }
         return -1;
     }
