8d464ae3e1e71ccb2d4eb4ee066e4b5cdf6fbb44
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-12 01:57:41 +0530

    Time: 24 ms (14.25%) | Memory: 60.1 MB (5.63%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,42 +1,33 @@
 class Solution {
-    private boolean canMake(int day, int[] bloomDay, int m, int k){
-        int totalBoq = 0,consecCount=0;
-        for(int i=0;i<bloomDay.length;i++){
-            if(bloomDay[i]<=day){
-                consecCount++; 
+    private boolean canMake(int day, int[] bloom, int m, int k){
+        int total=0, curr=0;
+        for(int i=0;i<bloom.length;i++){
+            if(day>=bloom[i]){
+                curr++;
+                if(curr==k){
+                    total++;
+                    curr=0;
+                }
             }else{
-                consecCount=0;
-            }
-            if(consecCount==k){
-                totalBoq++;
-                consecCount=0;
-
+                curr=0;
             }
         }
-
-        return totalBoq>=m;
+        return total>=m;
     }
-    public int minDays(int[] bloomDay, int m, int k) {
-        int n = bloomDay.length;
-        if((long)(m*k)>n){
-            return -1;
-        }
-        int end = Arrays.stream(bloomDay).max().getAsInt();
-        int start = Arrays.stream(bloomDay).min().getAsInt();
+    public int minDays(int[] bloom, int m, int k) {
+        int start = Arrays.stream(bloom).min().getAsInt();
+        int end = Arrays.stream(bloom).max().getAsInt();
+        if((long)(m*k)>bloom.length)    return -1;
         int ans=-1;
-
         while(start<=end){
-            int mid = start + (end-start)/2;
-
-            if(canMake(mid,bloomDay, m,k)){
+            int mid = (start+end)/2;
+            if(canMake(mid, bloom, m,k)){
                 ans=mid;
-                end = mid-1;
+                end=mid-1;
             }else{
                 start=mid+1;
             }
-
         }
         return ans;
-
     }
 }
