6abac66ed3fcc34f10b215f1fb7d199b96da4dfe
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-11 09:32:23 +0530

    Time: 5 ms (93.06%) | Memory: 87 MB (50.36%) - LeetSync

--- 3822-fill-a-special-grid/fill-a-special-grid.java
+++ 3822-fill-a-special-grid/fill-a-special-grid.java
@@ -1,26 +1,23 @@
 class Solution {
-    int[][] ans;
     int nums;
-    private void fillGrid(int r, int c, int size){
-        if(size==1){
-            ans[r][c] = --nums;
+    int[][] matrix;
+    private void dfs(int r, int c, int n){
+        if(n==1){
+            matrix[r][c] =--nums;
             return;
         }
-        int n = size/2;
-        fillGrid(r, c, n);
-        fillGrid(r+n, c, n);
-        fillGrid(r+n, c+n, n);
-        fillGrid(r, c+n, n);
-
+        n=n/2;
+        dfs(r, c, n);
+        dfs(r+n, c, n);
+        dfs(r+n, c+n, n);
+        dfs(r, c+n, n);
     }
+
     public int[][] specialGrid(int n) {
-        int size = (int) Math.pow(2, n) ;
-        ans = new int[size][size];
-        nums=size*size;
-
-        fillGrid(0,0,size);
-        return ans;
-
-        
+        int size =(int) Math.pow(2, n);
+        nums = size*size;
+        matrix = new int[size][size];
+        dfs(0,0, size);
+        return matrix;
     }
 }
