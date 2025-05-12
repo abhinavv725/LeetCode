339dd4cb44c523701a90664de4f226a7fe607888
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-12 18:39:13 +0530

    Time: 38 ms (80.66%) | Memory: 57.6 MB (51.21%) - LeetSync

--- 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.java
+++ 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.java
@@ -1,34 +1,14 @@
 class Solution {
-    private int solve(int idx, int[] arr, int d){
-        int result=0;
-        int prev=arr[idx];
-        for(int j=idx+1;j<arr.length;j++){
-            int curr = arr[j];
-            if(curr-prev==d){
-                result = Math.max(result, 1+solve(j, arr, d));
-            }
+    public int longestSubsequence(int[] arr, int difference) {
+        int n=arr.length;
+        HashMap<Integer, Integer> dp = new HashMap<>();
+        int ans=1;
+        for(int a:arr){
+            int before_a = a-difference;
+            int len = dp.getOrDefault(before_a, 0)+1;
+            dp.put(a, len);
+            ans = Math.max(ans, len);
         }
-        return result;
-
+        return ans;
     }
-    public int longestSubsequence(int[] arr, int d) {
-        int result=1;
-    //     for(int i=0;i<arr.length;i++){
-    //         result = Math.max(result, 1+solve(i, arr, difference));
-    //     }
-    //     return result;
-    
-    HashMap<Integer, Integer> map = new HashMap<>();
-    for(int i=0;i<arr.length;i++){
-        int curr = arr[i];
-        if(map.containsKey(curr-d)){
-            map.put(curr, map.get(curr-d)+1);
-            result = Math.max(result, map.get(curr));
-        }else{
-            map.put(curr, 1);
-            
-        }
-    }
-    return result;
-}
 }
