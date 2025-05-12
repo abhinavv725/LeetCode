0b970521edc4da93905540a62c57c8881cb3b201
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-13 02:04:04 +0530

    Time: 1 ms (6.64%) | Memory: 40.7 MB (96.31%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,27 +1,22 @@
-import java.util.*;
 class Solution {
-    
-    public int solve(int[] nums,int[] dp, int i){
-        
-        if(i==0)return nums[i];
-        if(i<0)return 0;
-        if(dp[i]!=-1)return dp[i];
-        
-        return dp[i]=Math.max(nums[i]+solve(nums, dp, i-2), 0+solve(nums,dp,i-1));
+    private int robHouse(int[] num, int start, int end){
+        int n = end-start+1;
+        int[] dp = new int[n];
+        dp[0] = num[start];
+        dp[1] = Math.max(num[start], num[start+1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-1], num[i+start]+dp[i-2]);
+        }
+        return Arrays.stream(dp).max().getAsInt();
     }
-    
     public int rob(int[] nums) {
-        int i = nums.length;
-        if(i==1)return nums[0];
-        int[] dp1 = new int[i+1];
-        Arrays.fill(dp1,-1);
-        int[] dp2 = new int[i+1];
-        Arrays.fill(dp2,-1);
-        
-        int m1= solve(Arrays.copyOfRange(nums,0,i-1), dp1, i-2);
-        int m2= solve(Arrays.copyOfRange(nums,1,i), dp2, i-2);
-        
-        return Math.max(m1,m2);
-        
+        int n=nums.length;
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        if (n == 2) return Math.max(nums[0], nums[1]);
+        int first = robHouse(nums, 0, n-2);
+        int second = robHouse(nums, 1, n-1);
+
+        return Math.max(first, second);
     }
 }
