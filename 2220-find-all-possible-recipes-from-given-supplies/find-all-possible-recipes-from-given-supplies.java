15fb0a84e6c33e55361ec117cfc8d5437cd009ab
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-12 16:40:21 +0530

    Time: 25 ms (97.34%) | Memory: 47.3 MB (10.26%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
@@ -1,34 +1,41 @@
 class Solution {
+    private boolean dfs(String recipe, Map<String, List<String>> graph ,
+            Set<String> supplySet ,Map<String, Boolean> canMake, Set<String> vis){
+            if(supplySet.contains(recipe))  return true;
+            if(canMake.containsKey(recipe)) return canMake.get(recipe);
+            if(!graph.containsKey(recipe))  return false;
+            if(vis.contains(recipe))        return false;
+            vis.add(recipe);
+            for(String ing: graph.get(recipe)){
+                if(!dfs(ing, graph, supplySet, canMake, vis)){
+                    vis.remove(recipe);
+                    canMake.put(recipe, false);
+                    return false;
+                }
+            }
+            vis.remove(recipe);
+            canMake.put(recipe, true);
+            return true;
+
+    }
+
+    
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
         List<String> ans = new ArrayList<>();
-        Set<String> supply = new HashSet<>();
-        for(String su: supplies){
-            supply.add(su);
+        Set<String> supplySet = new HashSet<>();
+        Map<String, Boolean> canMake = new HashMap<>();
+        for(String s: supplies){
+            supplySet.add(s);
         }
-        boolean progress=true;
-        while(progress){
-            progress=false;
-            for(int i=0;i<recipes.length;i++){
-                String r = recipes[i];
-                if(ans.contains(r)) continue;
-                List<String> ing = ingredients.get(i);
-                boolean canMake = true;
-                for(String ingre: ing){
-                    if(!supply.contains(ingre)){
-                        canMake=false;
-                        break;
-                    }
-                }
-                if(canMake){
-                    supply.add(r);
-                    ans.add(r);
-                    progress=true;
-                }
-
+        Map<String, List<String>> graph = new HashMap<>();
+        for(int i=0;i<recipes.length;i++){
+            graph.put(recipes[i], ingredients.get(i));
+        }
+        for(String recipe: recipes){
+            if(dfs(recipe, graph, supplySet, canMake, new HashSet<>())){
+                ans.add(recipe);
             }
-
         }
-        
 
         return ans;
     }
