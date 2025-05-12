07081093d55923bbd8fc230f35fe4501fed5b3dd
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-12 15:34:53 +0530

    Time: 570 ms (5.33%) | Memory: 46.5 MB (84.32%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
@@ -1,38 +1,35 @@
 class Solution {
-    private boolean checkRecepie( List<String> ingredients, HashSet<String> sup){
-        for(String i: ingredients){
-            if(!sup.contains(i)){
-                return false;
-            }
-        }
-
-        return true;
-    }
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        HashSet<String> sup = new HashSet<>();
-
-        for(String s: supplies){
-            sup.add(s);
-        }
         List<String> ans = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<recipes.length;i++){
-            q.add(i);
+        Set<String> supply = new HashSet<>();
+        for(String su: supplies){
+            supply.add(su);
         }
-        int lastSize=-1;
-        while(sup.size()> lastSize){
-            lastSize=sup.size();
-            int length = q.size();
-            while(length-- > 0){
-                int recipeId = q.poll();
-                if(checkRecepie(ingredients.get(recipeId), sup)){
-                    ans.add(recipes[recipeId]);
-                    sup.add(recipes[recipeId]);
-                }else{
-                    q.add(recipeId);
+        boolean progress=true;
+        while(progress){
+            progress=false;
+            for(int i=0;i<recipes.length;i++){
+                String r = recipes[i];
+                if(ans.contains(r)) continue;
+                List<String> ing = ingredients.get(i);
+                boolean canMake = true;
+                for(String ingre: ing){
+                    if(!supply.contains(ingre)){
+                        canMake=false;
+                        break;
+                    }
                 }
+                if(canMake){
+                    supply.add(r);
+                    ans.add(r);
+                    progress=true;
+                }
+
             }
+
         }
+        
+
         return ans;
     }
 }
