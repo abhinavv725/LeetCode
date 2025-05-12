4aeb42129ec4e32af29519fdc0904d0447cc75db
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-12 19:14:38 +0530

    Time: 510 ms (5.02%) | Memory: 45.4 MB (92.52%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,32 +1,13 @@
 class Solution {
-    private boolean jump(int i, int[] nums){
-        if(i==nums.length)
-            return false;
-        if(i==nums.length-1)
-            return true;
-        
-        for(int j=1;j<=nums[i];j++){
-            if(jump(i+j, nums)){
-                return true;
-            }
-        }
-        return false;
-    }
-    private boolean jumpTab(int[] nums){
-        int n= nums.length;
+    public boolean canJump(int[] nums) {
+        int n=nums.length;
         boolean[] dp = new boolean[n];
         dp[0]=true;
         for(int i=0;i<n;i++){
-            for(int j=1;j<=nums[i];j++){
-                if(i+j==n)
-                    break;
-                dp[i+j]=dp[i];
+            for(int j=i+1;j<=Math.min(n-1, i+nums[i]);j++){
+                dp[j]=dp[i];
             }
         }
-
         return dp[n-1];
-    }
-    public boolean canJump(int[] nums) {
-        return jumpTab(nums);
     }
 }
