bedf2a682e2cfe12b9fd481cadd1ae309ff4c06f
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-13 00:55:34 +0530

    Time: 24 ms (87.47%) | Memory: 43.2 MB (12.60%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -1,40 +1,20 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
 
 class Solution {
-    private Node clone(Node node, HashMap<Node, Node>map){
-        if(node==null)
-            return null;
+    private Node dfs(Node node, HashMap<Node, Node> map){
+        if(node==null)  return null;
         Node newNode = new Node(node.val);
         map.put(node, newNode);
         for(Node nei: node.neighbors){
             if(map.containsKey(nei)){
                 newNode.neighbors.add(map.get(nei));
             }else{
-                newNode.neighbors.add(clone(nei, map));
+                newNode.neighbors.add(dfs(nei, map));
             }
         }
         return newNode;
     }
     public Node cloneGraph(Node node) {
         HashMap<Node, Node> map = new HashMap<>();
-        return clone(node, map);
+        return dfs(node, map);
     }
 }
