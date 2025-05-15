014b8a04822e8d03a986da52c436a2203a1f531c
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-16 02:39:51 +0530

    Time: 6 ms (86.51%) | Memory: 45.8 MB (24.20%) - LeetSync

--- 3825-apply-substitutions/apply-substitutions.java
+++ 3825-apply-substitutions/apply-substitutions.java
@@ -1,24 +1,42 @@
 class Solution {
+    // Map to store variable replacements
+    HashMap<String, String> map;
+
     public String applySubstitutions(List<List<String>> replacements, String text) {
-        String ans="";
-        boolean check=true;
-        HashMap<String, String> map = new HashMap<>();
-        for(List<String> s: replacements){
-            map.put("%"+s.get(0)+"%", s.get(1));
+        map = new HashMap<>();
 
+        // Build the map from the list of replacements
+        for (List<String> r : replacements) {
+            map.put(r.get(0), r.get(1));
         }
-        while(check){
-            check=false;
-            for(String key: map.keySet()){
-                if(text.contains(key)){
-                    text=text.replace(key, map.get(key));
-                    check=true;
 
-                }
+        // Recursively resolve the input text
+        return dfs(text);
+    }
 
+    // Recursively resolves a string by replacing all %x% placeholders
+    private String dfs(String s) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0, n = s.length();
+
+        while (i < n) {
+            if (s.charAt(i) != '%') {
+                // Regular character, just append
+                sb.append(s.charAt(i));
+                i++;
+            } else {
+                // Found a placeholder: format is always %x%
+                int j = i + 2; // j points to the second '%'
+                String key = String.valueOf(s.charAt(i + 1)); // extract single-char key
+                String replacement = map.get(key); // get mapped value
+
+                // Recursively resolve this replacement in case it has placeholders
+                sb.append(dfs(replacement));
+
+                i = j + 1; // move past the closing '%'
             }
         }
 
-        return text;
+        return sb.toString();
     }
 }
