4956b3234a634e27a43b69623e7bb1868fbaf44f
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-15 21:04:50 +0530

    Time: 1 ms (58.41%) | Memory: 41.7 MB (57.54%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,29 +1,29 @@
 class Solution {
-    private boolean check(String[] nums, String ans){
-        for(String num: nums){
-            if(ans.equals(num)){
-                return false;
-            }
-        }
-        return true;
+    private boolean check(String s, Set<String> set){
+        return set.contains(s);
     }
-    private boolean perm(StringBuilder sb, String[] nums, int i, int n){
-        if(i>=n)
-            return false;
-        sb.setCharAt(i, '1');
-        if(check(nums, sb.toString()) || perm(sb, nums, i+1, n)){
-            return true;
+
+    private boolean perm(int i, StringBuilder sb, Set<String> set, int n){
+        if(i>=n){
+            return !check(sb.toString(), set);
         }
         sb.setCharAt(i, '0');
+        if(perm(i+1, sb, set, n)){
+            return true;
+        }
+        sb.setCharAt(i, '1');
+        if(perm(i+1, sb, set, n))
+            return true;
         return false;
     }
+
     public String findDifferentBinaryString(String[] nums) {
-        int n = nums.length;
+        Set<String> set = new HashSet<>();
+        for(String s: nums) set.add(s);
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<n;i++){
-            sb.append('0');
-        }
-        perm(sb, nums, 0, n);
+        for(int i=0;i<nums.length;i++)  sb.append('0');
+
+        perm(0, sb, set, nums.length);
         return sb.toString();
 
     }
