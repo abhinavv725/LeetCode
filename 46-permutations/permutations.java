8d96c03f6bf14210b9f4db4b26fb3abebe7abcb9
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-18 03:29:12 +0530

    Time: 1 ms (93.47%) | Memory: 44.3 MB (96.60%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -3,7 +3,7 @@
     List<List<Integer>> ans;
 
     // Recursive DFS function to build permutations
-    private void dfs(int[] nums, List<Integer> temp, boolean[] used) {
+    private void dfs(int[] nums, List<Integer> temp) {
         // Base case: if the temporary list has the same length as input, it's a valid permutation
         if (temp.size() == nums.length) {
             ans.add(new ArrayList<>(temp)); // Add a deep copy to the result
@@ -13,27 +13,23 @@
         // Iterate through each number in the input array
         for (int i = 0; i < nums.length; i++) {
             // Skip the number if it's already used in the current path
-            if (used[i]) continue;
+            if(temp.contains(nums[i]))  continue;
 
             // Choose the current number
-            used[i] = true;
             temp.add(nums[i]);
 
             // Explore further with the current number added
-            dfs(nums, temp, used);
+            dfs(nums, temp);
 
             // Backtrack: remove the last number and mark it as unused
             temp.remove(temp.size() - 1);
-            used[i] = false;
         }
     }
 
     public List<List<Integer>> permute(int[] nums) {
         ans = new ArrayList<>(); // Initialize the result list
 
-        boolean[] used = new boolean[nums.length]; // Tracks used numbers by index
-
-        dfs(nums, new ArrayList<>(), used); // Start DFS with an empty list
+        dfs(nums, new ArrayList<>()); // Start DFS with an empty list
 
         return ans; // Return all collected permutations
     }
