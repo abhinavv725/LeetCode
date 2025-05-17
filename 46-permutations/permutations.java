11589e3908919f058a12aecd6fc2384ccc86c48f
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-18 03:26:11 +0530

    Time: 1 ms (93.47%) | Memory: 45.1 MB (5.98%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,25 +1,40 @@
-class Solution {
+ class Solution {
+    // This will hold all the permutations
     List<List<Integer>> ans;
-    private void dfs(int[] nums, List<Integer> temp,boolean[] used ){
-        if(temp.size()==nums.length){
-            ans.add(new ArrayList<>(temp));
+
+    // Recursive DFS function to build permutations
+    private void dfs(int[] nums, List<Integer> temp, boolean[] used) {
+        // Base case: if the temporary list has the same length as input, it's a valid permutation
+        if (temp.size() == nums.length) {
+            ans.add(new ArrayList<>(temp)); // Add a deep copy to the result
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(used[i]) continue;
-            used[i]=true;
+
+        // Iterate through each number in the input array
+        for (int i = 0; i < nums.length; i++) {
+            // Skip the number if it's already used in the current path
+            if (used[i]) continue;
+
+            // Choose the current number
+            used[i] = true;
             temp.add(nums[i]);
+
+            // Explore further with the current number added
             dfs(nums, temp, used);
-            temp.remove(temp.size()-1);
-            used[i]=false;
+
+            // Backtrack: remove the last number and mark it as unused
+            temp.remove(temp.size() - 1);
+            used[i] = false;
         }
     }
-    public List<List<Integer>> permute(int[] nums) {
-        ans = new ArrayList<>();
-        boolean[] used = new boolean[nums.length];
-        dfs(nums, new ArrayList<>(), used);
 
-        return ans;
-        
+    public List<List<Integer>> permute(int[] nums) {
+        ans = new ArrayList<>(); // Initialize the result list
+
+        boolean[] used = new boolean[nums.length]; // Tracks used numbers by index
+
+        dfs(nums, new ArrayList<>(), used); // Start DFS with an empty list
+
+        return ans; // Return all collected permutations
     }
 }
