b262115d66724bf284caa769eff3912bcfe8e51e
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-18 22:28:26 +0530

    Time: 14 ms (64.17%) | Memory: 56.7 MB (74.02%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -1,26 +1,23 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for(int i=0;i<nums.size();i++){
-            map.put(nums.get(i), map.getOrDefault(nums.get(i), 0)+1);
+        int count=0, candidate=-1;
+        for(int n: nums){
+            if(count==0)
+                candidate=n;
+            count += (n==candidate) ? 1:-1; 
         }
-        int d =0, df=0;
-        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
-            if(entry.getValue() > df){
-                df=entry.getValue();
-                d=entry.getKey();
-            }
+        count=0;
+        for(int n: nums){
+            count += (n==candidate) ?1:0;
         }
-        
-        int countLeft=0;
+        int currCount = 0;
         for(int i=0;i<nums.size();i++){
-            if(nums.get(i)==d)
-                countLeft++;
-
-            int rightCount = df-countLeft;
-            if(countLeft*2 > (i+1) && rightCount*2>(nums.size()-(i+1))){
-                return i;
+            if(nums.get(i)==candidate){
+                currCount++;
+                int secondHalf = count-currCount;
+                if(currCount *2>(i+1) && secondHalf *2> (nums.size()-i-1)){
+                    return i;
+                }
             }
         }
         return -1;
