38fcd927a4045a3b7425572c453265d12182011b
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-18 16:55:50 +0530

    Time: 10 ms (19.94%) | Memory: 43.2 MB (5.16%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,27 +1,23 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        List<Integer> ans = new ArrayList<>();
-        HashMap<Character, Integer> last = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         for(int i=0;i<s.length();i++){
-            last.put(s.charAt(i), i);
+            map.put(s.charAt(i), i);
         }
-        int i=0, prev=-1;
-        while(i<s.length()){
+        List<Integer> ans = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        int maxIndex = 0;
+        for(int i=0;i<s.length();i++){
             char curr = s.charAt(i);
-            int lastIndex = last.get(curr);
-            int j=i;
-            while(j!=lastIndex){
-                char temp =s.charAt(j);
-                lastIndex = Math.max(lastIndex, last.get(temp));
-                j++;
+            sb.append(curr);
+            maxIndex = Math.max(maxIndex, map.get(curr));
+            if(i==maxIndex){
+                ans.add(sb.length());
+                sb = new StringBuilder();
             }
-
-            ans.add(j-i+1);
-            i=j+1;
+            
         }
-
-
-
         return ans;
+        
     }
 }
