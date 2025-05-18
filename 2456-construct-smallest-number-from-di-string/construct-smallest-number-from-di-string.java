b279ef88c45657856d3ca642254a310f79cc9b3d
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-18 15:51:09 +0530

    Time: 1 ms (80.71%) | Memory: 41.5 MB (87.04%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
@@ -1,55 +1,24 @@
 class Solution {
-    
-    private boolean recursion(String pattern, int j,StringBuilder ans,boolean[] used){
-        int last =  ans.charAt(ans.length()-1) - '0';
-        if(j>=pattern.length())
-            return true;
-        if(pattern.charAt(j)==('I')){
-            if(last==9){
-                return false;
-            }
-            for(int i= last +1;i<=9 ;i++){
-                if(!used[i]){
-                    ans.append(i);
-                    used[i]=true;
-                    if(recursion(pattern, j+1, ans, used)){
-                        return true;
-                    }
-                    ans.deleteCharAt(ans.length()-1);
-                    used[i]=false;
-                }
-            }
-        }else{
-            if(last==1){
-                return false;
-            }
-            for(int i= last-1;i>=1 ;i--){
-                if(!used[i]){
-                    ans.append(i);
-                    used[i]=true;
-                    if(recursion(pattern, j+1, ans, used)){
-                        return true;
-                    }
-                    ans.deleteCharAt(ans.length()-1);
-                    used[i]=false;
-                }
+    public String check(String pattern) {
+        StringBuilder result = new StringBuilder();
+        Deque<Integer> stack = new ArrayDeque<>();
 
+        int n = pattern.length();
+        for (int i = 0; i <= n; i++) {
+            // Push i+1 because digits start from 1
+            stack.push(i + 1);
+
+            // If end of pattern or current char is 'I', flush the stack
+            if (i == n || pattern.charAt(i) == 'I') {
+                while (!stack.isEmpty()) {
+                    result.append(stack.pop());
+                }
             }
         }
-        return false;
+
+        return result.toString();
     }
     public String smallestNumber(String pattern) {
-        
-        for(int i=1;i<=9;i++){
-            StringBuilder ans = new StringBuilder();
-            boolean[] used = new boolean[10];
-            used[i]=true;
-            ans.append(i);
-            if(recursion(pattern, 0, ans, used)){
-                return ans.toString();
-            }
-            ans.deleteCharAt(ans.length()-1);
-        }
-        return "";
+        return check(pattern);
     }
 }
