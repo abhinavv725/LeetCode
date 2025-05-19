96afbe362e91e15ec2cc2581bacd0689e3572580
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-19 19:10:12 +0530

    Time: 4 ms (51.55%) | Memory: 41.6 MB (73.54%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,49 +1,39 @@
-class Solution {
-    private class Node {
-        char ch;
-        int freq;
-        Node(char ch, int freq){
-            this.ch = ch;
-            this.freq=freq;
-        }
+class Pair {
+    char c; int freq;
+    Pair(char c, int freq){
+        this.c=c;
+        this.freq=freq;
     }
+}
+class Solution {
+     
     public String reorganizeString(String s) {
-        PriorityQueue<Node> pq = new PriorityQueue<>(
-            (a,b) -> Integer.compare(b.freq, a.freq)
-        );
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c: s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> Integer.compare(b.freq, a.freq));
+        for(char c: map.keySet()){
+            pq.add(new Pair(c, map.get(c)));
+        }
         StringBuilder sb = new StringBuilder();
-        int[] charCount = new int[26];
-        for(char c : s.toCharArray()){
-            charCount[c - 'a']++;
+        while(pq.size()>=2){
+            Pair first = pq.poll();
+            Pair second = pq.poll();
+            sb.append(first.c);
+            sb.append(second.c);
+            if(--first.freq>0)  pq.add(first);
+            if(--second.freq>0)  pq.add(second);
         }
-        for(int i=0;i<26;i++){
-            if(charCount[i]>0){
-                pq.add(new Node((char)(i+'a'), charCount[i]));
-
+        if(!pq.isEmpty()){
+            Pair last = pq.poll();
+            if(last.freq>1){
+                return "";
             }
-        }
-        while(!pq.isEmpty()){
-            Node one = pq.poll();
-            if(pq.isEmpty()){
-                if(one.freq==1){
-                    sb.append(one.ch);
-                    return sb.toString();
-                }else{
-                    return "";
-                }
-            }
-            Node two = pq.poll();
-            sb.append(one.ch);
-            sb.append(two.ch);
-            one.freq--;
-            two.freq--;
-            if(one.freq>0){
-                pq.add(one);
-            }
-            if(two.freq>0){
-                pq.add(two);
-            }
+            sb.append(last.c);
         }
         return sb.toString();
+
+
     }
 }
