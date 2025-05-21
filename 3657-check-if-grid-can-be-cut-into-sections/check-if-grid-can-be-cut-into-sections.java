23a3e80fb7e7ed524a778d27df71a489ef2ec20e
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-22 01:34:52 +0530

    Time: 89 ms (75.42%) | Memory: 119.3 MB (57.52%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.java
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.java
@@ -1,19 +1,19 @@
 class Solution {
-    public boolean checkValidCuts(int n, int[][] rectangles) {
-        return check(rectangles, 0) || check(rectangles, 1);
-    }
-    private boolean check(int[][] rectangles, int d){
+    private boolean validate(int[][] rectangles, int d){
         int gap=0;
-        Arrays.sort(rectangles, (a,b)-> Integer.compare(a[d], b[d]));
-
-        int farthest = rectangles[0][d+2];
+        Arrays.sort(rectangles, (a,b) -> Integer.compare(a[d], b[d]));
+        int farthest = rectangles[0][2+d];
         for(int i=1;i<rectangles.length;i++){
-            int[] rect = rectangles[i];
-            if(farthest <=rect[d]){
+            int start = rectangles[i][d];
+            int end = rectangles[i][2+d];
+            if(farthest <= start){
                 gap++;
             }
-            farthest = Math.max(farthest, rect[d+2]);
-        } 
+            farthest = Math.max(farthest, end);
+        }
         return gap>=2;
+    }
+    public boolean checkValidCuts(int n, int[][] rectangles) {
+        return validate(rectangles, 0) || validate(rectangles , 1);
     }
 }
