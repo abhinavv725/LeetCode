c9114e663d94e4399bf4210dffffc018aea7a213
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-21 20:36:55 +0530

    Time: 15 ms (16.06%) | Memory: 48.7 MB (27.93%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    private int getCount(int key, int[] nums){
+        int ans=0;
+        for(int n: nums){
+            if(n==key)  ans++;
+        }
+        return ans;
+    }
+    public List<Integer> majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int n: nums){
+            map.put(n, map.getOrDefault(n, 0)+1);
+            if(map.size()>2){
+                HashMap<Integer, Integer> temp = new HashMap<>();
+
+                for(int key: map.keySet()){
+                    int count = map.get(key) - 1;
+                    if (count > 0) {
+                        temp.put(key, count);
+                    }
+                }
+                map=temp;
+
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+
+        for(int key: map.keySet()){
+            if(getCount(key, nums)> nums.length/3){
+                ans.add(key);
+            }
+        }
+        
+        return ans;
+    }
+}
