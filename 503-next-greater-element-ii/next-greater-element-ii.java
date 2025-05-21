2465c8342db9e40a7793c0ca1dc20ba5b2f37733
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-21 20:15:28 +0530

    Time: 18 ms (28.80%) | Memory: 46.3 MB (50.59%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,26 +1,30 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        int[] arr = new int[n*2];
         int[] res = new int[n];
-        Arrays.fill(res, -1);
-        for(int i=0;i< (2*n);i++){
-            arr[i] = nums[i%n];
-        }
-        // 1  2 1 1  2  1
-        // 2 -1 2 2 -1 -1      
-
         Stack<Integer> st = new Stack<>();
-        for(int i=(2*n)-1;i>=0;i--){
-            while(!st.isEmpty() && arr[st.peek()] <= arr[i]){
+
+        // Initialize result with -1
+        Arrays.fill(res, -1);
+
+        // Traverse the array twice in reverse to simulate circularity
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int index = i % n;
+
+            // Pop elements from stack that are smaller than or equal to current
+            while (!st.isEmpty() && nums[st.peek()] <= nums[index]) {
                 st.pop();
             }
-            if(i< n && !st.isEmpty()){
-                res[i] = arr[st.peek()];
-            }
-            st.push(i);
 
+            // If stack is not empty, the top is the next greater element
+            if (!st.isEmpty()) {
+                res[index] = nums[st.peek()];
+            }
+
+            // Push current index onto the stack
+            st.push(index);
         }
+
         return res;
     }
 }
