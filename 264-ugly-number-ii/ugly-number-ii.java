c1ba03a380593ceb59550882c388d13b3b44d68e
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-05-23 05:26:42 +0530

    Time: 52 ms (21.68%) | Memory: 45.2 MB (7.07%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -1,14 +1,14 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        TreeSet<Long> ts = new TreeSet<>();
-        ts.add(1L);
+        TreeSet<Long> num = new TreeSet<>();
+        num.add(1L);
         long curr=0;
-        while(n-- >0 ){
-            curr = ts.pollFirst();
-            ts.add(curr*2);
-            ts.add(curr*3);
-            ts.add(curr*5);
+        while(n-- > 0){
+            curr = num.pollFirst();
+            num.add(curr*2);
+            num.add(curr*3);
+            num.add(curr*5);
         }
-        return  (int)curr;
+        return (int) curr;
     }
 }
