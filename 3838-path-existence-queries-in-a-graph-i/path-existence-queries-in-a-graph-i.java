5b20248a55fca46e531cbf1d3a5be0858b40c759
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-03 16:42:38 +0530

    Time: 14 ms (37.08%) | Memory: 83.3 MB (47.09%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
@@ -1,42 +1,52 @@
-class Solution {
+class DSU {
     int[] parent;
     int[] rank;
-    private int find(int x){
-        if(parent[x]==-1){
+    DSU(int n){
+        rank = new int[n];
+        parent= new int[n];
+        Arrays.fill(rank, 1);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+    }
+
+    public int find(int x){
+        if(x==parent[x]){
             return x;
         }
-        return parent[x]=find(parent[x]);
+        return parent[x] = find(parent[x]);
     }
-    private boolean union(int a, int b){
-        int rankA = find(a);
-        int rankB = find(b);
-        if(rankA==rankB)
+    public boolean union(int i, int j){
+        if(isConnected(i, j)){
             return false;
-        
-        if(rankA> rankB){
-            parent[rankB] =rankA;
-            rank[rankA]+=rank[rankB];
-        }else{
-            parent[rankA] =rankB;
-            rank[rankB]+=rank[rankA];
         }
-        
+        int rootA = find(i), rootB = find(j);
+        if(rank[rootA]>rank[rootB]){
+            parent[rootB]=rootA;
+            rank[rootA]+=rank[rootB];
+        }else{
+            parent[rootA]=rootB;
+            rank[rootB]+=rank[rootA];
+        }
         return true;
     }
-    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
-        parent = new int[n];
-        rank=new int[n];
-        Arrays.fill(parent, -1);
+    public boolean isConnected(int i, int j){
+        return find(i)==find(j);
+    }
+}
 
+class Solution {
+    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        DSU dsu = new DSU(n);
         for(int i=1;i<n;i++){
             if(Math.abs(nums[i-1]-nums[i])<=maxDiff){
-                union(i-1, i);
+                dsu.union(i-1, i);
             }
         }
         boolean[] ans = new boolean[queries.length];
         int i=0;
         for(int[]q: queries){
-            ans[i++] = find(q[0]) == find(q[1]);
+            ans[i++] = dsu.isConnected(q[0], q[1]);
         }
         return ans;
     }
