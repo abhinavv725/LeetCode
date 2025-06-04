1d2c1a28feeac58019d0b37ad85a27f16d2521ef
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-04 16:09:16 +0530

    Time: 111 ms (30.44%) | Memory: 80.7 MB (62.84%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.java
+++ 1962-single-threaded-cpu/single-threaded-cpu.java
@@ -1,34 +1,37 @@
 class Solution {
     public int[] getOrder(int[][] tasks) {
         int n = tasks.length;
-        int[][] taskTime = new int[n][3];
+        int[] ans = new int[n];
+        int time=0;
+        int[][] arr = new int[n][3];
         for(int i=0;i<n;i++){
-            taskTime[i][0] = tasks[i][0]; //enqueue
-            taskTime[i][1] = tasks[i][1]; // processing time
-            taskTime[i][2] = i; // index
+            int[] t = tasks[i];
+            arr[i][0] = t[0]; //enqueue
+            arr[i][1] = t[1]; //processing
+            arr[i][2] = i; //index
         }
-        Arrays.sort(taskTime, (a, b)->Integer.compare(a[0], b[0]));
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a,b) -> (a[1]==b[1]) ? 
-                Integer.compare(a[2], b[2]) : 
-                Integer.compare(a[1], b[1])
-        );
-        int[] result = new int[n];
-        int idx=0, time=0, i=0;
-
+        Arrays.sort(arr,(a, b)-> Integer.compare(a[0], b[0]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)-> {
+            if(a[1]==b[1]){
+                return Integer.compare(a[2], b[2]);
+            }
+            return Integer.compare(a[1], b[1]);
+        });
+        int i =0, idx=0;
         while(i<n || !pq.isEmpty()){
-            while(i<n && taskTime[i][0]<=time){
-                pq.add(taskTime[i]);
+            while(i<n && time>= arr[i][0]){
+                pq.add(arr[i]);
                 i++;
             }
             if(!pq.isEmpty()){
                 int[] curr = pq.poll();
-                result[idx++] = curr[2];
-                time+=curr[1];
+                ans[idx++] = curr[2];
+
+                time += curr[1];
             }else{
-                time=taskTime[i][0];
+                time=arr[i][0];
             }
         }
-        return result;
+        return ans;
     }
 }
