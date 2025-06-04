92d86126365425aa83a12350a6b6c43094ab88fc
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-04 12:52:56 +0530

    Time: 423 ms (5.00%) | Memory: 46.1 MB (6.25%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,37 +1,45 @@
 class Solution {
-    private boolean isMatch(HashMap<Character, Integer> m1, HashMap<Character, Integer> m2){
-        for(char c: m1.keySet()){
-            if(!m2.containsKey(c) || m2.get(c) < m1.get(c)){
+    private boolean checkEqual(HashMap<Character, Integer> m1,HashMap<Character, Integer> m2 ){
+        
+        for(char k: m1.keySet()){
+            if(m2.getOrDefault(k, 0)<m1.get(k))
                 return false;
-            } 
         }
+        
         return true;
     }
     public String minWindow(String s, String t) {
+        if(t.length()> s.length())
+            return "";
+        
         HashMap<Character, Integer> m1 = new HashMap<>();
         HashMap<Character, Integer> m2 = new HashMap<>();
+
         for(char c: t.toCharArray()){
             m1.put(c, m1.getOrDefault(c, 0)+1);
         }
-        int l=0; 
-        String ans ="";
+        int l=0;
+        String ans="";
         for(int r=0;r<s.length();r++){
             char c = s.charAt(r);
-            if(m1.containsKey(c))
+            if(m1.containsKey(c)){
                 m2.put(c, m2.getOrDefault(c, 0)+1);
-            
-            while(l<=r && isMatch(m1, m2)){
-                if(ans.length() == 0 || (r-l+1) < ans.length()){
-                    ans = s.substring(l, r+1);
+            }
+            while(l<=r && checkEqual(m1, m2)){
+                String temp = s.substring(l, r+1);
+                if(ans.length()==0 || (r-l+1)<ans.length()){
+                    ans=temp;
                 }
-                char x = s.charAt(l);
-                if(m1.containsKey(x)){
-                    m2.put(x, m2.get(x)-1);
-                    if(m2.get(x)==0){
-                        m2.remove(x);
+                c = s.charAt(l);
+                if(m2.containsKey(c)){
+                    m2.put(c, m2.get(c)-1);
+                    if(m2.get(c)==0){
+                        m2.remove(c);
                     }
+
                 }
                 l++;
+                
             }
         }
 
