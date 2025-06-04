32ae2cbb339c89e470ae2ace19f32d98a80da928
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-04 17:35:00 +0530

    Time: 1 ms (81.33%) | Memory: 42 MB (16.44%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
@@ -1,24 +1,18 @@
 class Solution {
-    public String check(String pattern) {
-        StringBuilder result = new StringBuilder();
-        Deque<Integer> stack = new ArrayDeque<>();
+    public String smallestNumber(String pattern) {
+        Stack<Integer> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        int n=pattern.length();
+        
+        for(int i=0;i<=n;i++){
+            st.push(i+1);
 
-        int n = pattern.length();
-        for (int i = 0; i <= n; i++) {
-            // Push i+1 because digits start from 1
-            stack.push(i + 1);
-
-            // If end of pattern or current char is 'I', flush the stack
-            if (i == n || pattern.charAt(i) == 'I') {
-                while (!stack.isEmpty()) {
-                    result.append(stack.pop());
+            if(i==n || pattern.charAt(i)=='I'){
+                while(!st.isEmpty()){
+                    sb.append(st.pop());
                 }
             }
         }
-
-        return result.toString();
-    }
-    public String smallestNumber(String pattern) {
-        return check(pattern);
+        return sb.toString();
     }
 }
