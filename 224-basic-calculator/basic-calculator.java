486eb94fae8bcecf728bc4dd95511e9612a93e6e
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-04 10:51:12 +0530

    Time: 9 ms (59.45%) | Memory: 43.6 MB (83.82%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,35 +1,34 @@
 class Solution {
-    Stack<int[]> st = new Stack<>();
     public int calculate(String s) {
-        int sign = 1;
-        int currNum =0, res=0;
+        Stack<int[]> st = new Stack<>();
+        int curr=0;
+        int sign = 1; //-1 or 1
+        int res=0;
         for(int i=0;i<=s.length();i++){
             if(i==s.length()){
-                res = res + (currNum*sign);
+                res+= (curr*sign);
                 break;
-
             }
             char c = s.charAt(i);
             if(c==' '){
                 continue;
+            }else if(c== '+' || c=='-'){
+                res += curr *sign;
+                curr = 0;
+                sign = (c=='+') ?1 :-1;
             }else if(Character.isDigit(c)){
-                currNum = 10*currNum + (c-'0');
-            }else if(c == '+' || c == '-'){
-                res = res + (currNum*sign);
-                currNum=0;
-                sign = (c == '+') ? 1: -1;
-            }else if(c == '('){
+                curr = curr *10 + (c-'0');
+            }else if(c=='('){
                 st.push(new int[] {res, sign});
-                currNum=0;
                 res=0;
-                sign=1;
-            }else if(c== ')'){
-                res = res + (currNum*sign);
-                currNum=0;
+                sign =1;
+                curr=0;
+            }else{
                 int[] last = st.pop();
-                res = last[0] + res*last[1];
+                res += (curr*sign);
+                curr=0;
+                res = last[0] +res*last[1];
             }
-            
 
         }
         return res;
