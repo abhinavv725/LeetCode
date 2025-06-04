7c9de5c64ccab35cf0268532ed70e72f5aa0aa45
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-04 15:15:40 +0530

    Time: 3 ms (99.61%) | Memory: 55 MB (81.64%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.java
+++ 1002-maximum-width-ramp/maximum-width-ramp.java
@@ -1,18 +1,19 @@
 class Solution {
     public int maxWidthRamp(int[] nums) {
-        int n = nums.length;
-        int[] right = new int[n];
-        right[n-1]=nums[n-1];
+        int ans=0, n = nums.length, l=0;
+        int[] right =new int[n];
+        right[n-1] = nums[n-1];
         for(int i=n-2;i>=0;i--){
             right[i] = Math.max(nums[i], right[i+1]);
         }
-        int l=0, res=0;
         for(int r=1;r<n;r++){
-            while(nums[l]>right[r]){
+            while(nums[l]> right[r]){
                 l++;
             }
-            res = Math.max(r-l, res);
+            ans=Math.max(ans, r-l);
         }
-        return res;
+
+
+        return ans;
     }
 }
