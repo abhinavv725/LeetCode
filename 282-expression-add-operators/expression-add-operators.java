84c2aa2006126988a343747904795b3fb79a3578
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-04 10:39:54 +0530

    Time: 114 ms (28.45%) | Memory: 45.8 MB (25.44%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.java
+++ 282-expression-add-operators/expression-add-operators.java
@@ -1,35 +1,32 @@
 class Solution {
     List<String> ans = new ArrayList<>();
-    private void dfs(int idx, String path, String num, int target, long evaluated, long prev){
+    private void dfs(int idx, long curr, int target, String num, String part, long prev){
         if(idx==num.length()){
-            if(evaluated == target){
-                ans.add(path);
+            if(curr==target){
+                ans.add(part);
             }
             return;
         }
-
         for(int i=idx;i<num.length();i++){
             if(i!=idx && num.charAt(idx)=='0')
                 break;
-            
-            String currSum = num.substring(idx, i+1);
-            long currNum = Long.parseLong(currSum);
-
-            if(idx == 0){
-                dfs(i+1, path + currSum, num, target, currNum, currNum);
+            String n = num.substring(idx, i+1);
+            long currNum = Long.parseLong(n);
+            if(idx==0){
+                dfs(i+1, currNum, target, num, n, currNum);
             }else{
-                dfs(i+1, path + "+" + currSum, num, target, evaluated+currNum, currNum);
-                dfs(i+1, path + "-" + currSum, num, target, evaluated-currNum, -currNum);
-
-                dfs(i+1, path + "*" + currSum, num, target,
-                 evaluated - prev + (prev* currNum), prev* currNum);
+                dfs(i+1, curr + currNum, target, num, part + "+" + currNum, currNum);
+                dfs(i+1, curr - currNum, target, num, part + "-" + currNum, -currNum);
+                dfs(i+1, (curr - prev) + (prev * currNum), target, num, part + "*" + currNum,prev* currNum);
             }
-
+            
         }
+        
 
     }
     public List<String> addOperators(String num, int target) {
-        dfs(0, "", num, target, 0, 0);
+
+        dfs(0, 0, target, num, "", 0);
         return ans;
     }
 }
