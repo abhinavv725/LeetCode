526aefaac3272f169276ebdd69b9038a33833540
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-05 13:17:56 +0530

    Time: 8 ms (66.44%) | Memory: 45.9 MB (88.05%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,8 +1,9 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
         Arrays.sort(intervals, (a,b)-> Integer.compare(a[0], b[0]));
-        PriorityQueue<Integer> pq= new PriorityQueue<>();
+
         int ans=0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         pq.add(intervals[0][1]);
         for(int i=1;i<intervals.length;i++){
             if(intervals[i][0]>=pq.peek()){
@@ -10,6 +11,7 @@ class Solution {
             }
             pq.add(intervals[i][1]);
         }
+
         return pq.size();
     }
 }
