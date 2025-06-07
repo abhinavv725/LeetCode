9ae0f495bb69ca1eb500ef2af5f43aaeeb63313b
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-07 14:18:36 +0530

    Time: 79 ms (87.94%) | Memory: 45 MB (57.55%) - LeetSync

--- 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
@@ -1,20 +1,20 @@
 class Solution {
-    private boolean inRange(int[] a, int[]b){
-        long dx = a[0]-b[0];
-        long dy =a[1]-b[1];
-        long distSqr = dx*dx + dy*dy;
-        long radius = (long)a[2]*a[2];
-        return distSqr<=radius;
+    private boolean inRange(int[] a, int[] b){
+        long dx = a[0] - b[0];
+        long dy = a[1] - b[1];
+        long distSqr = dx * dx + dy * dy;                 // Euclidean distance squared
+        long radius = (long) a[2] * a[2];                 // Square of the blast radius
+        return distSqr <= radius;     
     }
-    private int dfs(int idx, List<List<Integer>> adj, boolean[] vis){
-        vis[idx] =true;
-        int count=1;
-        for(int nei: adj.get(idx)){
+    private int dfs(int node,List<List<Integer>> adj , boolean[] vis){
+        vis[node]=true;
+        int ans=0;
+        for(int nei: adj.get(node)){
             if(!vis[nei]){
-                count += dfs(nei, adj, vis);
+                ans+= dfs(nei, adj, vis);
             }
         }
-        return count;
+        return 1+ans;
     }
     public int maximumDetonation(int[][] bombs) {
         List<List<Integer>> adj = new ArrayList<>();
@@ -22,7 +22,6 @@ class Solution {
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i!=j && inRange(bombs[i], bombs[j])){
@@ -30,14 +29,10 @@ class Solution {
                 }
             }
         }
-        int ans = 0;
-
+        int ans=0;
         for(int i=0;i<n;i++){
-            boolean[] vis = new boolean[n];
-            ans = Math.max(ans, dfs(i,adj,vis));
-
+            ans = Math.max(ans, dfs(i,adj,  new boolean[n]));
         }
         return ans;
-        
     }
 }
