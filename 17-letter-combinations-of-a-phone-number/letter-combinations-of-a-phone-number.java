4b5dab5dd1b80a4e4ff2da01b4e7393dcf7e2b6e
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-07 15:54:37 +0530

    Time: 5 ms (26.54%) | Memory: 42.4 MB (37.66%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,42 +1,28 @@
 class Solution {
-    List<String> ans;
-    HashMap<Character, String> map;
-    StringBuilder sb;
-    Solution(){
-        ans = new ArrayList<>();
-        map = new HashMap<>();
-        sb = new StringBuilder();
-        map.put('2',"abc");
-        map.put('3',"def");
-        map.put('4',"ghi");
-        map.put('5',"jkl");
-        map.put('6',"mno");
-        map.put('7',"pqrs");
-        map.put('8',"tuv");
-        map.put('9',"wxyz");
-
-    }
-    private void dfs(int i, String digits){
-        if(i==digits.length()){
-            if(sb.length()!=0)
-                ans.add(sb.toString());
+    List<String> ans = new ArrayList<>();
+    HashMap<Character, String> map = new HashMap<>();
+    private void dfs(int idx, String curr, String digits){
+        if(idx==digits.length()){
+            if(!curr.equals(""))
+                ans.add(curr);
             return;
         }
-        char curr = digits.charAt(i);
-        String keys = map.get(curr);
-        for(char c: keys.toCharArray()){
-            sb.append(c);
-            dfs(i+1, digits);
-
-            sb.deleteCharAt(sb.length()-1);
-            // dfs(i+1, digits);
-
+        String val = map.get(digits.charAt(idx));
+        for(char c: val.toCharArray()){
+            dfs(idx+1, curr+c, digits);
         }
-
+        
     }
-
     public List<String> letterCombinations(String digits) {
-        dfs(0, digits);
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+        dfs(0,"", digits);
         return ans;
     }
 }
