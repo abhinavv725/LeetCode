70dbfb81dc36ccf043065f779ee4ce917059ebac
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-09 21:45:05 +0530

    Time: 37 ms (79.27%) | Memory: 100.6 MB (46.58%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.java
+++ 3430-count-days-without-meetings/count-days-without-meetings.java
@@ -1,29 +1,20 @@
 class Solution {
     public int countDays(int days, int[][] meetings) {
-        Arrays.sort(meetings, (a,b)->Integer.compare(a[0],b[0]));
-        int i=0;
-        List<int[]> arr = new ArrayList<>();
-        while(i<meetings.length){
-            int[] first = meetings[i];
-            int j=i+1;
-            while(j<meetings.length && first[1]>=meetings[j][0]){
-                first[0] = Math.min(first[0],meetings[j][0] );
-                first[1] = Math.max(first[1],meetings[j][1]);
-                j++;
-            }
-            arr.add(first);
-            i=j;
-        }
-        int freedays=0;
-        for( i=0;i<arr.size();i++){
-            if(i==0){
-                freedays += arr.get(i)[0]-1;
-            }else{
-                freedays+= arr.get(i)[0]-arr.get(i-1)[1]-1;
-            }
-        }
-        freedays+= (days - arr.get(arr.size()-1)[1]);
+        Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
 
-        return freedays;
+        int lastEnd = 0, ans=0;
+        for(int i=0;i<meetings.length;i++){
+            int[] meet = meetings[i];
+            if(lastEnd+1<meet[0]){
+                ans+=(meet[0] - lastEnd-1);
+            }
+            lastEnd = Math.max(lastEnd, meet[1]);
+
+        }
+        if(lastEnd<days){
+            ans+=(days-lastEnd);
+        }
+
+        return ans;
     }
 }
