5a39bd985a64892607055ba7c82c026a47d3473f
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-11 10:30:04 +0530

    Time: 27 ms (60.17%) | Memory: 57.4 MB (14.92%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,6 +1,35 @@
 class Solution {
+    public int[] mergeSort(int[] nums){
+        int n = nums.length;
+        if(n<=1)    return nums;
+        int mid = n/2;
+        int[] l = Arrays.copyOfRange(nums, 0, mid);
+        int[] r = Arrays.copyOfRange(nums, mid, n);
+
+        l = mergeSort(l);
+        r = mergeSort(r);
+        return merge(l,r);
+    }
+    public int[] merge(int[] a, int[] b){
+        int n=a.length, m=b.length;
+        int[] ans = new int[n+m];
+        int i=0, j=0, idx=0;
+        while(i<n && j<m){
+            if(a[i]<=b[j]){
+                ans[idx++] = a[i++];
+            }else{
+                ans[idx++] = b[j++];
+            }
+        }
+        while(i<n){
+            ans[idx++] =a[i++];
+        }
+        while(j<m){
+            ans[idx++]=b[j++];
+        }
+        return ans;
+    }
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
-        return nums;
+        return mergeSort(nums);
     }
 }
