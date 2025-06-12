46a60287117f317b7cff38d7b112ac302f83b7cb
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-12 16:41:07 +0530

    Time: 9 ms (60.47%) | Memory: 43.8 MB (53.41%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,36 +1,35 @@
 class Solution {
     public int calculate(String s) {
-        Stack<int[]> st = new Stack<>();
-        int curr=0;
-        int sign = 1; //-1 or 1
-        int res=0;
-        for(int i=0;i<=s.length();i++){
-            if(i==s.length()){
-                res+= (curr*sign);
-                break;
-            }
+        int num=0;
+        int sign =1, res=0;
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
             if(c==' '){
                 continue;
-            }else if(c== '+' || c=='-'){
-                res += curr *sign;
-                curr = 0;
-                sign = (c=='+') ?1 :-1;
             }else if(Character.isDigit(c)){
-                curr = curr *10 + (c-'0');
-            }else if(c=='('){
-                st.push(new int[] {res, sign});
+                num = num*10 + (c-'0');
+            }else if(c=='+'){
+                res+=(num*sign);
+                num=0;sign=1;
+            }else if(c=='-'){
+                res+=(num*sign);
+                num=0;sign=-1;
+            
+            }else if(c =='('){
+                st.push(res);
+                st.push(sign);
                 res=0;
-                sign =1;
-                curr=0;
-            }else{
-                int[] last = st.pop();
-                res += (curr*sign);
-                curr=0;
-                res = last[0] +res*last[1];
-            }
+                sign = 1;
 
+            }else if(c==')'){
+                res += (sign*num);
+                res *= st.pop();
+                res+=st.pop();
+                num=0;
+                sign=1;
+            }
         }
-        return res;
+        return res + sign*num;
     }
 }
