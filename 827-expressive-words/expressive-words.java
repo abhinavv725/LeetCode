9256cd8be572bd18fd8694ee3da50720d9a1d4e6
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-14 23:40:06 +0530

    Time: 1 ms (100.00%) | Memory: 42 MB (56.15%) - LeetSync

--- 827-expressive-words/expressive-words.java
+++ 827-expressive-words/expressive-words.java
@@ -1,34 +1,31 @@
 class Solution {
-    private boolean canBeExpressed(String a, String b){
-        int i=0, j=0;
-        int n =a.length(), m=b.length();
-        while(i<n && j<m){
-            if(a.charAt(i)!=b.charAt(j)){
-                return false;
-            }
-            int aStart =i;
-            int bStart=j;
-            while(i<n && a.charAt(i)==a.charAt(aStart))
-                i++;
-            
-            while(j<m && b.charAt(j)==b.charAt(bStart))
-                j++;
-            int aCount = i-aStart;
-            int bCount = j-bStart;
-            if(bCount>aCount)
-                return false;
-            if (aCount != bCount && aCount < 3) return false;
 
-            
+    private boolean check(String s, String w){
+        int i=0, j=0;
+        while(i<s.length() && j<w.length()){
+            int c1=0, c2=0;
+            if(s.charAt(i)!=w.charAt(j))    return false;
+            char a1 = s.charAt(i);
+            while(i<s.length() && a1==s.charAt(i)){
+                i++;c1++;
+            }
+            while(j<w.length() && a1==w.charAt(j)){
+                j++;c2++;
+            }
+            if(c1==c2)  continue;
+            if(c2> c1)  return false;
+            if(c1> c2 && c1<3)  return false;
         }
-        return i==n && j==m;
+
+        return i==s.length() && j==w.length(); 
     }
+
+
     public int expressiveWords(String s, String[] words) {
         int ans=0;
-        for(String word: words){
-            if(canBeExpressed(s, word)){
-                ans++;
-            }
+
+        for(String w:words){
+            if(check(s, w)) ans++;
         }
         return ans;
     }
