84fa8cd16cb71130da9ecbc066942736661612b9
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2025-06-14 17:04:30 +0530

    Time: 226 ms (16.11%) | Memory: 97.6 MB (32.89%) - LeetSync

--- 3786-longest-palindromic-subsequence-after-at-most-k-operations/longest-palindromic-subsequence-after-at-most-k-operations.java
+++ 3786-longest-palindromic-subsequence-after-at-most-k-operations/longest-palindromic-subsequence-after-at-most-k-operations.java
@@ -1,26 +1,22 @@
 class Solution {
-    private int dfs(int l, int r, int operations, String s, int[][][] dp){
+    Integer[][][] dp;
+    private int dfs(int l, int r, String s, int ops){
+        if(ops<0)   return Integer.MIN_VALUE;
         if(l==r)    return 1;
-        if(l>r)     return 0;
-        if(dp[l][r][operations]!=-1)    return dp[l][r][operations];
-        int moveL = dfs(l+1, r, operations-0, s, dp);
-        int moveR = dfs(l, r-1, operations-0, s,dp);
-        int convert=Integer.MIN_VALUE;
-        int opsRequired = Math.min(Math.abs(s.charAt(l)-s.charAt(r)),
-                                    26 - Math.abs(s.charAt(l)-s.charAt(r)));
-        if(opsRequired<=operations){
-            convert = 2+ dfs(l+1, r-1, operations-opsRequired, s, dp);
+        if(l>r) return 0;
+        if(dp[l][r][ops]!=null){
+            return dp[l][r][ops];
         }
-        return dp[l][r][operations]=Math.max(moveL, Math.max(moveR, convert));
+
+        int m1 = dfs(l+1, r, s, ops);
+        int m2 = dfs(l, r-1, s, ops);
+        int x = Math.abs(s.charAt(l) - s.charAt(r));
+        int m3 = 2+ dfs(l+1, r-1, s, ops - Math.min(x, 26-x));
+        return dp[l][r][ops]= Math.max(m1, Math.max(m2, m3));
+ 
     }
     public int longestPalindromicSubsequence(String s, int k) {
-        int n=s.length();
-        int[][][] dp = new int[n][n][k+1];
-        for(int[][] d2: dp){
-            for(int[]d1: d2){
-                Arrays.fill(d1, -1);
-            }
-        }
-        return dfs(0, n-1, k, s, dp);
+        dp=new Integer[s.length()][s.length()][k+1];
+        return dfs(0, s.length()-1, s, k);
     }
 }
