a8b4126343a2ebe71be48e02bac676da75fe3723
Abhinav Verma <52631391+abhinavv725@users.noreply.github.com>
2026-02-20 19:01:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (57.82%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -1,29 +1,31 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
+    /**
+     * Time Complexity: O(L) - where L is the length of the linked list. We traverse it once.
+     * Space Complexity: O(1) - We only use two constant-space pointers.
+     */
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        // Create a dummy node to handle edge cases like removing the head
         ListNode dummy = new ListNode(0);
-        dummy.next=head;
-        ListNode slow =dummy, fast=dummy;
+        dummy.next = head;
         
-        for(int i=0;i<n;i++){
-            fast=fast.next;
-        }
-        
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next;
-        }
-        slow.next=slow.next.next;
-        return dummy.next;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
 
+        // Move fast pointer n+1 steps ahead so there's a gap of n nodes between fast and slow
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        // Move both pointers until fast reaches the end
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        // slow.next is the nth node from the end; skip it
+        slow.next = slow.next.next;
+
+        // Return the actual head (which might have changed)
+        return dummy.next;
     }
 }
